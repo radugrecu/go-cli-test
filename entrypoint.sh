7ec0eefdc0bc7738e7c62d5d9b4d02193574da0c
Radu GRECU <radugrecu@users.noreply.github.com>
2024-03-10 19:17:32 +0000

    attempt to run the cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,31 @@ if [ -z "${INPUT_GITHUB_TOKEN}" ] ; then
 fi
 
 ls -la
+
+function get_release_assets {
+  repo="$1"
+  version="$2"
+  args=(
+    -sSL
+    --header "Accept: application/vnd.github+json"
+  )
+  [ -n "${INPUT_GITHUB_TOKEN}" ] && args+=(--header "Authorization: Bearer ${INPUT_GITHUB_TOKEN}")
+  curl "${args[@]}" "https://api.github.com/repos/$repo/releases${version}" | jq '.assets[] | { name: .name, download_url: .browser_download_url }'
+}
+
+function install_release {
+  repo="$1"
+  version="$2"
+  binary="$3-linux-amd64"
+  checksum="$4"
+  release_assets="$(get_release_assets "${repo}" "${version}")"
+
+  curl -sLo "${binary}" "$(echo "${release_assets}" | jq -r ". | select(.name == \"${binary}\") | .download_url")"
+  curl -sLo "$3-checksums.txt" "$(echo "${release_assets}" | jq -r ". | select(.name | contains(\"$checksum\")) | .download_url")"
+
+  grep "${binary}" "$3-checksums.txt" | sha256sum -c -
+  install "${binary}" "/usr/local/bin/${3}"
+}
+
+install_release radugrecu/go-cli-test "${COMMENTER_VERSION}" commenter checksums.txt
+go-cli-test ${INPUT_REPORT_FILE}
