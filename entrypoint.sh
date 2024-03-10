2dff9441b27909fb19e56640f5dd0c5892700b2c
Radu GRECU <radugrecu@users.noreply.github.com>
2024-03-10 18:51:36 +0000

    update inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-echo "Never gonna give you up, never gonna let you down"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+echo "using file ${INPUT_REPORT_FILE}"
 
-echo "who-to-greet: ${INPUT_who-to-greet}"
-echo "another_input: ${INPUT_another_input}"
-
-printenv | grep INPUT
-
-echo "INPUT_WHO-TO-GREET: ${INPUT_WHO-TO-GREET}"
-echo "INPUT_ANOTHER_INPUT: ${INPUT_ANOTHER_INPUT}"
+# time=$(date)
+# echo "time=$time" >> $GITHUB_OUTPUT
