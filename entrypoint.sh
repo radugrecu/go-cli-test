c545f72a729f103b0b6fc34e523d6e69edc6f13b
Radu GRECU <radugrecu@users.noreply.github.com>
2024-03-10 18:42:17 +0000

    testing action inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,5 @@ echo "Never gonna give you up, never gonna let you down"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
+echo "who-to-greet: ${INPUT_who-to-greet}"
+echo "another_input: ${INPUT_another_input}
