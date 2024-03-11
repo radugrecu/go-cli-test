30d8fa731d3d7fdd68d45dfd9a0e5dad5f348c45
Radu GRECU <radugrecu@users.noreply.github.com>
2024-03-11 05:03:25 +0000

    comment to PR

--- go.mod
+++ go.mod
@@ -5,9 +5,20 @@ go 1.22
 require github.com/aquasecurity/trivy v0.49.1
 
 require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-github/v32 v32.1.0 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/oauth2 v0.15.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
+)
+
+require (
 	github.com/anchore/go-struct-converter v0.0.0-20221118182256-c68fdcfa2092 // indirect
 	github.com/aquasecurity/trivy-db v0.0.0-20231005141211-4fc651f7ac8d // indirect
 	github.com/google/go-containerregistry v0.19.0 // indirect
+	github.com/owenrumney/go-github-pr-commenter v0.1.3
 	github.com/package-url/packageurl-go v0.1.2 // indirect
 	github.com/samber/lo v1.39.0 // indirect
 	github.com/spdx/tools-golang v0.5.4-0.20231108154018-0c0f394b5e1a // indirect
--- main.go
+++ main.go
@@ -3,36 +3,130 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
+	"net/url"
 	"os"
 	"strconv"
+	"strings"
 
 	trivyTypes "github.com/aquasecurity/trivy/pkg/types"
+	"github.com/owenrumney/go-github-pr-commenter/commenter"
 )
 
 func main() {
+	fmt.Println("Starting the github commenter")
 
-	println("1 starting go-cli-test")
-	fmt.Println("2 starting go-cli-test")
-	log.Println("3 starting go-cli-test")
+	token := os.Getenv("INPUT_GITHUB_TOKEN")
+	if len(token) == 0 {
+		fail("the INPUT_GITHUB_TOKEN has not been set")
+	}
+
+	githubRepository := os.Getenv("GITHUB_REPOSITORY")
+	split := strings.Split(githubRepository, "/")
+	if len(split) != 2 {
+		fail(fmt.Sprintf("unexpected value for GITHUB_REPOSITORY. Expected <organisation/name>, found %v", split))
+	}
+	owner := split[0]
+	repo := split[1]
+
+	fmt.Printf("Working in repository %s\n", repo)
+
+	prNo, err := extractPullRequestNumber()
+	if err != nil {
+		fmt.Println("Not a PR, nothing to comment on, exiting")
+		return
+	}
+	fmt.Printf("Working in PR %v\n", prNo)
 
 	args := os.Args[1:]
-
 	reportPath := "trivy_sample_report.json"
 	if len(args) > 0 {
 		reportPath = args[0]
 	}
-	trivyReport := loadTrivyReport(reportPath)
-	printTrivyReport(trivyReport)
+	trivyReport, err := loadTrivyReport(reportPath)
+	if err != nil {
+		fail(fmt.Sprintf("failed to load trivy report: %s", err.Error()))
+	}
+	if len(trivyReport.Results) == 0 {
+		fmt.Println("No results found in trivy report, exiting")
+		os.Exit(0)
+	}
+	fmt.Printf("Trivy found %v issues\n", len(trivyReport.Results))
+
+	c, err := createCommenter(token, owner, repo, prNo)
+	if err != nil {
+		fail(fmt.Sprintf("failed to create commenter: %s", err.Error()))
+	}
+
+	workspacePath := fmt.Sprintf("%s/", os.Getenv("GITHUB_WORKSPACE"))
+	fmt.Printf("Working in GITHUB_WORKSPACE %s\n", workspacePath)
+
+	workingDir := os.Getenv("INPUT_WORKING_DIRECTORY")
+	if workingDir != "" {
+		workingDir = strings.TrimPrefix(workingDir, "./")
+		workingDir = strings.TrimSuffix(workingDir, "/") + "/"
+	}
+
+	var errMessages []string
+	var validCommentWritten bool
+	for _, result := range trivyReport.Results {
+		// skip non config/terraform results
+		if result.Class != "config" && result.Type != "terraform" {
+			fmt.Printf("%s / %s / %s - not a config/terraform result; skipping", result.Target, result.Type, result.Class)
+			continue
+		}
+		// skip if no misconfigurations
+		if len(result.Misconfigurations) == 0 {
+			fmt.Printf("%s / %s / %s - no misconfigurations; skipping", result.Target, result.Type, result.Class)
+			continue
+		}
+
+		for _, misconfiguration := range result.Misconfigurations {
+			filename := workingDir + strings.ReplaceAll(result.Target, workspacePath, "")
+			comment := generateErrorMessage(misconfiguration)
+			fmt.Printf("Preparing comment for violation of rule %v in %v\n", misconfiguration.ID, filename)
+			err := c.WriteMultiLineComment(filename, comment, misconfiguration.CauseMetadata.StartLine, misconfiguration.CauseMetadata.EndLine)
+			if err != nil {
+				switch err.(type) {
+				case commenter.CommentAlreadyWrittenError:
+					fmt.Println("Ignoring - comment already written")
+					validCommentWritten = true
+				case commenter.CommentNotValidError:
+					fmt.Println("Ignoring - change not part of the current PR")
+					continue
+				default:
+					errMessages = append(errMessages, err.Error())
+				}
+			} else {
+				validCommentWritten = true
+				fmt.Printf("Comment written for violation of rule %v in %v\n", misconfiguration.ID, filename)
+			}
+		}
+	}
+
+	if len(errMessages) > 0 {
+		fmt.Printf("There were %d errors:\n", len(errMessages))
+		for _, err := range errMessages {
+			fmt.Println(err)
+		}
+		os.Exit(1)
+	}
+
+	if validCommentWritten || len(errMessages) == 0 {
+		if softFail, ok := os.LookupEnv("INPUT_SOFT_FAIL_COMMENTER"); ok && strings.ToLower(softFail) == "true" {
+			return
+		}
+		os.Exit(1)
+	}
 
 }
 
-func loadTrivyReport(reportPath string) trivyTypes.Report {
+func loadTrivyReport(reportPath string) (trivyTypes.Report, error) {
 	fmt.Println("Loading trivy report from " + reportPath)
 
 	file, err := os.Open(reportPath)
 	if err != nil {
-		log.Fatal(err)
+		return trivyTypes.Report{}, err
 	}
 	defer file.Close()
 
@@ -40,23 +134,43 @@ func loadTrivyReport(reportPath string) trivyTypes.Report {
 	decoder := json.NewDecoder(file)
 	err = decoder.Decode(&report)
 	if err != nil {
-		log.Fatal(err)
+		return trivyTypes.Report{}, err
 	}
 
 	fmt.Println("Trivy report loaded successfully")
 
-	return report
+	return report, nil
+}
+
+func createCommenter(token, owner, repo string, prNo int) (*commenter.Commenter, error) {
+	var err error
+	var c *commenter.Commenter
+
+	githubApiUrl := os.Getenv("GITHUB_API_URL")
+	if githubApiUrl == "" || githubApiUrl == "https://api.github.com" {
+		c, err = commenter.NewCommenter(token, owner, repo, prNo)
+	} else {
+		url, err := url.Parse(githubApiUrl)
+		if err == nil {
+			enterpriseUrl := fmt.Sprintf("%s://%s", url.Scheme, url.Hostname())
+			c, err = commenter.NewEnterpriseCommenter(token, enterpriseUrl, enterpriseUrl, owner, repo, prNo)
+		}
+	}
+
+	return c, err
 }
 
 func printTrivyReport(report trivyTypes.Report) {
+	fmt.Println("Printing trivy report")
 	for _, result := range report.Results {
 		// skip non config/terraform results
 		if result.Class != "config" && result.Type != "terraform" {
-			log.Printf("%s / %s / %s - not a config/terraform result; skipping", result.Target, result.Type, result.Class)
+			fmt.Printf("%s / %s / %s - not a config/terraform result; skipping", result.Target, result.Type, result.Class)
 			continue
 		}
 		// skip if no misconfigurations
 		if len(result.Misconfigurations) == 0 {
+			fmt.Printf("%s / %s / %s - no misconfigurations; skipping", result.Target, result.Type, result.Class)
 			continue
 		}
 
@@ -92,4 +206,31 @@ func writeMultiLineComment(filename string, comment string, startline int, endli
 	println("Comment: " + comment)
 	println("Startline: " + strconv.Itoa(startline) + " Endline: " + strconv.Itoa(endline))
 	println("--- END COMMENT ---")
+}
+
+func extractPullRequestNumber() (int, error) {
+	github_event_file := "/github/workflow/event.json"
+	file, err := ioutil.ReadFile(github_event_file)
+	if err != nil {
+		fail(fmt.Sprintf("GitHub event payload not found in %s", github_event_file))
+		return -1, err
+	}
+
+	var data interface{}
+	err = json.Unmarshal(file, &data)
+	if err != nil {
+		return -1, err
+	}
+	payload := data.(map[string]interface{})
+
+	prNumber, err := strconv.Atoi(fmt.Sprintf("%v", payload["number"]))
+	if err != nil {
+		return 0, fmt.Errorf("not a valid PR")
+	}
+	return prNumber, nil
+}
+
+func fail(err string) {
+	fmt.Printf("Error: %s\n", err)
+	os.Exit(-1)
 }
